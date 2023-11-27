869426c9075436a394bd4feafedbf48dfd67fa71
brian <yatunyi.15075@students.kyu.ac.ke>
2023-11-27 18:58:21 +0300

    adding files

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,11 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <div>
-      <p>
-        This is Protected 
-      </p>
-      
-      
+      <UserButton afterSignOutUrl="/"
+      />
     </div>
   )
 }
